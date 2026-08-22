b29352b34685462b22f4ffd7081a936ca82b5680
webmonologue <webmonologue@gmail.com>
2026-08-22 18:51:49 +0900

    Add sitemap integration, site URL, and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://insightbrewer.com',
+  integrations: [sitemap()],
+});
